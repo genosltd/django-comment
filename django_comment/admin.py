ad797e7b38fab9726643f6e231d6538e9fbe064a
Genadij Razdorov <genadijrazdorov@gmail.com>
2021-08-09 15:20:28 +0200

    Init models and admin

--- django_comment/admin.py
+++ django_comment/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
 
-# Register your models here.
+from django.db.models.functions import Concat
+from django.db.models import Value
+
+from . import models
+
+
+@admin.register(models.CommentedItem)
+class CommentedItemAdmin(admin.ModelAdmin):
+    list_display = ('item', 'author', 'posted_on', 'comment')
+    ordering = ('posted_on',)
+    readonly_fields = ('content_object', 'author', 'posted_on')
+
+    def item(self, obj):
+        return obj.content_object
+
+    item.admin_order_field = Concat('content_type', Value(' '), 'object_id')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        else:
+            return False
+
+
+class CommentedItemInline(GenericStackedInline):
+    model = models.CommentedItem
+    readonly_fields = ('author', 'posted_on')
+    fields = ('author', 'posted_on', 'comment')
+    extra = 1
+    can_delete = False
+    ordering = ('posted_on',)
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class HasCommentsAdmin(admin.ModelAdmin):
+    inlines = (CommentedItemInline,)
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for obj in formset.new_objects:
+            obj.author = request.user
+            obj.save()
+        super().save_formset(request, form, formset, change)
--- django_comment/models.py
+++ django_comment/models.py
@@ -1,3 +1,28 @@
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class CommentedItem(models.Model):
+    class Meta:
+        verbose_name = 'comment'
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE, editable=False)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     editable=False)
+    object_id = models.PositiveIntegerField(editable=False)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return f"{self.content_object} commented by {self.author} on " \
+                    f"{self.posted_on}"
+
+
+class HasComments(models.Model):
+    comments = GenericRelation(CommentedItem)
